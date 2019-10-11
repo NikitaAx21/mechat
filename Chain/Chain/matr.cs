8973b2a5e302bcdc5e7fbb8195a808a23420b36c
muralev <muralev.aleksei@gmail.com>
2019-10-11 22:31:19 +0500

    Добавил два файла. Поменял возвращающий параметр одного из методов на Point. Матрица преобразования теперь без использования Vector. Значения предыдущего просто сохраняются в классе в переменных x y

---
+++ Chain/Chain/matr.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chain
+{
+    public static class DegreeToRad
+    {
+        
+        public static double DegreeToRadian(double A)
+        {
+            return (Math.PI * A / 180.0);
+        }
+    }
+    public class TransformationMatrix
+    {
+        public double[,] matr = new double[3, 3];
+        double curAngle = 0;
+        public double X = 0; //для хранения координаты конца предыдущего сегмента
+        public double Y = 0;
+
+        public TransformationMatrix()
+        {
+            matr[0, 0] = 0;
+            matr[0, 1] = 0;
+            matr[0, 2] = 1;
+            matr[1, 0] = 0;
+            matr[1, 1] = 0;
+            matr[1, 2] = 1;
+            matr[2, 0] = 0;
+            matr[2, 1] = 0;
+            matr[2, 2] = 1;
+        }
+        public TransformationMatrix(double angle)
+        {
+            curAngle = curAngle + angle;
+            double rad = DegreeToRad.DegreeToRadian(curAngle); //Поменять везде
+            matr[0, 0] = Math.Cos(rad);
+            matr[0, 1] = Math.Sin(rad);
+            matr[0, 2] = X; //исправить на length 
+            matr[1, 0] = -Math.Sin(rad);
+            matr[1, 1] = Math.Cos(rad);
+            matr[1, 2] = Y;
+            matr[2, 0] = 0;
+            matr[2, 1] = 0;
+            matr[2, 2] = 1;
+        }
+
+    }
+    public class CoordMatrix
+    {
+        public double[] vect = new double[3];
+        public CoordMatrix()
+        {
+            vect[0] = 1;
+            vect[1] = 1;
+            vect[2] = 1;
+        }
+        public CoordMatrix(Segment S, Joint J)
+        {
+            vect[0] = S.Length * Math.Sin(DegreeToRad.DegreeToRadian(J.CurrentAngle));
+            vect[1] = S.Length * Math.Cos(DegreeToRad.DegreeToRadian(J.CurrentAngle));
+            vect[2] = 1;
+        }
+        
+
+    }
+    public class RotateMatrix
+    {
+        public double[,] matr = new double[3, 3];
+        public RotateMatrix(double angle)
+        {
+            double rad = DegreeToRad.DegreeToRadian(angle);
+            matr[0, 0] = Math.Cos(rad);
+            matr[0, 1] = Math.Sin(rad);
+            matr[0, 2] = 0;
+            matr[1, 0] = -Math.Sin(rad);
+            matr[1, 1] = Math.Cos(rad);
+            matr[1, 2] = 0;
+            matr[2, 0] = 0;
+            matr[2, 1] = 0;
+            matr[2, 2] = 1;
+        }
+        
+    }
+
+}
---
+++ Chain/Chain/matrix.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Chain
+{
+    class Matrix
+    {
+        double dX;
+        double dY;
+        public double[,] matr = new double[3, 3];
+        public double[] vect= new double[3];
+        
+        /*Метод для вычисления координат в Лаб.С.К.
+          В метод передается матрица преобразования и вектор координат в собственной с.к.
+          Возвращается вектор из двух элементов [x,y]
+        */
+        //Поменять на Point
+        public Point LabCoord(TransformationMatrix tM, CoordMatrix cM)
+        {
+            Point res = new Point();
+            res.X = tM.matr[0, 0] * cM.vect[0] + tM.matr[0, 1] * cM.vect[1] + tM.matr[0, 2] * cM.vect[2];
+            res.Y = tM.matr[1, 0] * cM.vect[0] + tM.matr[1, 1] * cM.vect[1] + tM.matr[1, 2] * cM.vect[2];
+            tM.X = res.X;
+            tM.Y = res.Y;
+            return res;
+        }
+        public Matrix(double x, double y)
+        {
+            dX = x;
+            dY = y;
+        }
+
+        /*Метод для поворота всех элементов на заданный угол a.
+          Считает координату (x,y)
+          Метод пока ничего не возвращает 
+        */
+        public void Rotate(List<Object> list, double a)  
+        {
+            RotateMatrix rM;
+            rM = new RotateMatrix(a);
+            for (int i = 0; i < list.Count; i++)
+            {
+                var S = list[i] as Segment;
+                if (S != null)
+                {
+                    S.Vector.X = S.Vector.X * rM.matr[0, 0] + S.Vector.Y * rM.matr[0, 1];
+                    S.Vector.Y = S.Vector.X * rM.matr[1, 0] + S.Vector.Y * rM.matr[1, 1];
+                }
+            }
+        }
+
+    }
+
+}
