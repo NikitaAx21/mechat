224a958bd74d2ec5c38274a6ee2bfd1d0f861da4
muralev <muralev.aleksei@gmail.com>
2019-10-12 21:57:32 +0500

    Убрал метод Rotate. Буду делать через один метод все. И нахождение координат нового сегмента и поворота всей системы.

--- Chain/Chain/matrix.cs
+++ Chain/Chain/matrix.cs
@@ -16,9 +16,8 @@ namespace Chain
         
         /*Метод для вычисления координат в Лаб.С.К.
           В метод передается матрица преобразования и вектор координат в собственной с.к.
-          Возвращается вектор из двух элементов [x,y]
+          Возвращается Point из двух элементов [x,y]
         */
-        //Поменять на Point
         public Point LabCoord(TransformationMatrix tM, CoordMatrix cM)
         {
             Point res = new Point();
@@ -27,30 +26,6 @@ namespace Chain
             tM.X = res.X;
             tM.Y = res.Y;
             return res;
-        }
-        public Matrix(double x, double y)
-        {
-            dX = x;
-            dY = y;
-        }
-
-        /*Метод для поворота всех элементов на заданный угол a.
-          Считает координату (x,y)
-          Метод пока ничего не возвращает 
-        */
-        public void Rotate(List<Object> list, double a)  
-        {
-            RotateMatrix rM;
-            rM = new RotateMatrix(a);
-            for (int i = 0; i < list.Count; i++)
-            {
-                var S = list[i] as Segment;
-                if (S != null)
-                {
-                    S.Vector.X = S.Vector.X * rM.matr[0, 0] + S.Vector.Y * rM.matr[0, 1];
-                    S.Vector.Y = S.Vector.X * rM.matr[1, 0] + S.Vector.Y * rM.matr[1, 1];
-                }
-            }
         }
 
     }
