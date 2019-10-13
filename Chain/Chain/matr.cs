4ffa3863e3639e15439c56f1b14d0d12f358ed04
Никита <nikita.axyonow@hotmail.com>
2019-10-14 00:45:15 +0500

    Рефакторинг. Модификор доступа.

--- Chain/Chain/matr.cs
+++ Chain/Chain/matr.cs
@@ -3,101 +3,99 @@ using System.Windows;
 
 namespace Chain
 {
-    public static class Calc
-    {
-        public static double DegreeToRadian(double A)
-        {
-            return Math.PI * A / 180.0;
-        }
-        public static bool IsIntersected(Point a, Point b, Point c, Point d)
-        {
-            double common = (b.X - a.X) * (d.Y - c.Y) - (b.Y - a.Y) * (d.X - c.X);
+	public static class Calc
+	{
+		public static double DegreeToRadian(double A)
+		{
+			return Math.PI * A / 180.0;
+		}
 
-            if (common == 0) return false; //они параллельны
+		public static bool IsIntersected(Point a, Point b, Point c, Point d)
+		{
+			var common = (b.X - a.X) * (d.Y - c.Y) - (b.Y - a.Y) * (d.X - c.X);
 
-            double rH = (a.Y - c.Y) * (d.X - c.X) - (a.X - c.X) * (d.Y - c.Y);
-            double sH = (a.Y - c.Y) * (b.X - a.X) - (a.X - c.X) * (b.Y - a.Y);
-            double r = rH / common;
-            double s = sH / common;
+			if (common == 0) return false; //они параллельны
 
-            if (r >= 0 && r <= 1 && s >= 0 && s <= 1)
-                return true;
-            else
-                return false;
-        }
-    }
+			var rH = (a.Y - c.Y) * (d.X - c.X) - (a.X - c.X) * (d.Y - c.Y);
+			var sH = (a.Y - c.Y) * (b.X - a.X) - (a.X - c.X) * (b.Y - a.Y);
+			var r = rH / common;
+			var s = sH / common;
 
-    public class TransformationMatrix
-    {
-        public double[,] matr = new double[3, 3];
-        private double curAngle = 0;
-        public double X = 0; //для хранения координаты конца предыдущего сегмента
-        public double Y = 0;
+			return r >= 0 && r <= 1 && s >= 0 && s <= 1;
+		}
+	}
 
-        public TransformationMatrix()
-        {
-            matr[0, 0] = 0;
-            matr[0, 1] = 0;
-            matr[0, 2] = 1;
-            matr[1, 0] = 0;
-            matr[1, 1] = 0;
-            matr[1, 2] = 1;
-            matr[2, 0] = 0;
-            matr[2, 1] = 0;
-            matr[2, 2] = 1;
-        }
+	public class TransformationMatrix
+	{
+		public double[,] Matrix = new double[3, 3];
+		private double _curAngle = 0;
+		public double X = 0; //для хранения координаты конца предыдущего сегмента
+		public double Y = 0;
 
-        public TransformationMatrix(double angle)
-        {
-            curAngle = curAngle + angle;
-            double rad = Calc.DegreeToRadian(curAngle); //Поменять везде
-            matr[0, 0] = Math.Cos(rad);
-            matr[0, 1] = Math.Sin(rad);
-            matr[0, 2] = X; //исправить на length 
-            matr[1, 0] = -Math.Sin(rad);
-            matr[1, 1] = Math.Cos(rad);
-            matr[1, 2] = Y;
-            matr[2, 0] = 0;
-            matr[2, 1] = 0;
-            matr[2, 2] = 1;
-        }
+		public TransformationMatrix()
+		{
+			Matrix[0, 0] = 0;
+			Matrix[0, 1] = 0;
+			Matrix[0, 2] = 1;
+			Matrix[1, 0] = 0;
+			Matrix[1, 1] = 0;
+			Matrix[1, 2] = 1;
+			Matrix[2, 0] = 0;
+			Matrix[2, 1] = 0;
+			Matrix[2, 2] = 1;
+		}
 
-    }
-    public class CoordMatrix
-    {
-        public double[] vect = new double[3];
-        public CoordMatrix()
-        {
-            vect[0] = 1;
-            vect[1] = 1;
-            vect[2] = 1;
-        }
-        public CoordMatrix(Segment S, Joint J)
-        {
-            vect[0] = S.Length * Math.Sin(Calc.DegreeToRadian(J.CurrentAngle));
-            vect[1] = S.Length * Math.Cos(Calc.DegreeToRadian(J.CurrentAngle));
-            vect[2] = 1;
-        }
+		public TransformationMatrix(double angle)
+		{
+			_curAngle += angle;
+			var rad = Calc.DegreeToRadian(_curAngle); //Поменять везде
+			Matrix[0, 0] = Math.Cos(rad);
+			Matrix[0, 1] = Math.Sin(rad);
+			Matrix[0, 2] = X; //исправить на length 
+			Matrix[1, 0] = -Math.Sin(rad);
+			Matrix[1, 1] = Math.Cos(rad);
+			Matrix[1, 2] = Y;
+			Matrix[2, 0] = 0;
+			Matrix[2, 1] = 0;
+			Matrix[2, 2] = 1;
+		}
+	}
 
+	public class CoordinatesMatrix
+	{
+		public double[] Vector = new double[3];
 
-    }
-    public class RotateMatrix
-    {
-        public double[,] matr = new double[3, 3];
-        public RotateMatrix(double angle)
-        {
-            double rad = Calc.DegreeToRadian(angle);
-            matr[0, 0] = Math.Cos(rad);
-            matr[0, 1] = Math.Sin(rad);
-            matr[0, 2] = 0;
-            matr[1, 0] = -Math.Sin(rad);
-            matr[1, 1] = Math.Cos(rad);
-            matr[1, 2] = 0;
-            matr[2, 0] = 0;
-            matr[2, 1] = 0;
-            matr[2, 2] = 1;
-        }
+		public CoordinatesMatrix()
+		{
+			Vector[0] = 1;
+			Vector[1] = 1;
+			Vector[2] = 1;
+		}
 
-    }
+		public CoordinatesMatrix(Segment s, Joint j)
+		{
+			Vector[0] = s.Length * Math.Sin(Calc.DegreeToRadian(j.CurrentAngle));
+			Vector[1] = s.Length * Math.Cos(Calc.DegreeToRadian(j.CurrentAngle));
+			Vector[2] = 1;
+		}
+	}
 
+	public class RotateMatrix
+	{
+		public double[,] Matrix = new double[3, 3];
+
+		public RotateMatrix(double angle)
+		{
+			var rad = Calc.DegreeToRadian(angle);
+			Matrix[0, 0] = Math.Cos(rad);
+			Matrix[0, 1] = Math.Sin(rad);
+			Matrix[0, 2] = 0;
+			Matrix[1, 0] = -Math.Sin(rad);
+			Matrix[1, 1] = Math.Cos(rad);
+			Matrix[1, 2] = 0;
+			Matrix[2, 0] = 0;
+			Matrix[2, 1] = 0;
+			Matrix[2, 2] = 1;
+		}
+	}
 }
--- Chain/Chain/matrix.cs
+++ Chain/Chain/matrix.cs
@@ -2,31 +2,31 @@
 
 namespace Chain
 {
-    class Matrix
-    {
-        private double dX;
-        private double dY;
-        public double[,] matr = new double[3, 3];
-        public double[] vect = new double[3];
+	public class Matrix
+	{
+		private double _dX;
+		private double _dY;
+		public double[,] matr = new double[3, 3];
+		public double[] vect = new double[3];
 
-        /// <summary>
-        /// Метод для вычисления координат в Лаб.С.К.
-        ///В метод передается матрица преобразования и вектор координат в собственной с.к.
-        ///Возвращается Point из двух элементов[x, y]
-        /// </summary>
-        /// <param name="tM"></param>
-        /// <param name="cM"></param>
-        /// <returns></returns>
-        public Point LabCoord(TransformationMatrix tM, CoordMatrix cM)
-        {
-            Point res = new Point();
-            res.X = tM.matr[0, 0] * cM.vect[0] + tM.matr[0, 1] * cM.vect[1] + tM.matr[0, 2] * cM.vect[2];
-            res.Y = tM.matr[1, 0] * cM.vect[0] + tM.matr[1, 1] * cM.vect[1] + tM.matr[1, 2] * cM.vect[2];
-            tM.X = res.X;
-            tM.Y = res.Y;
-            return res;
-        }
-
-    }
-
+		/// <summary>
+		/// Метод для вычисления координат в Лаб.С.К.
+		///В метод передается матрица преобразования и вектор координат в собственной с.к.
+		///Возвращается Point из двух элементов[x, y]
+		/// </summary>
+		/// <param name="tM"></param>
+		/// <param name="cM"></param>
+		/// <returns></returns>
+		public Point LabCoord(TransformationMatrix tM, CoordinatesMatrix cM)
+		{
+			var res = new Point
+			{
+				X = tM.Matrix[0, 0] * cM.Vector[0] + tM.Matrix[0, 1] * cM.Vector[1] + tM.Matrix[0, 2] * cM.Vector[2],
+				Y = tM.Matrix[1, 0] * cM.Vector[0] + tM.Matrix[1, 1] * cM.Vector[1] + tM.Matrix[1, 2] * cM.Vector[2]
+			};
+			tM.X = res.X;
+			tM.Y = res.Y;
+			return res;
+		}
+	}
 }
