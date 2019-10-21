70ae2d1849cceeb2c6f26527795e7eb60b568836
Пётр Козьминых <p.kozminih@mail.ru>
2019-10-21 18:52:59 +0600

    Исправлено взаимодействие с невидимыми объектами

---
+++ Chain/Chain/BoolToOpacityConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Chain
+{
+    public class BoolToOpacityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                bool x = (bool)value;
+
+                return x ? 1 : 0;
+
+            }
+            else
+            {
+
+                return value == null ? 1 : 0;
+
+            }
+
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
